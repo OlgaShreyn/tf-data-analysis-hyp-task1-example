3e5eea4031d35b93e6413d10b867833c970f53ad
OlgaShreyn <57268388+OlgaShreyn@users.noreply.github.com>
2023-04-13 22:13:17 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,11 +9,8 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 0.06
-    u = stats.norm.ppf((1-alpha)/2+0.5)
-    p_x = x_success / x_cnt
-    p_y = y_success / y_cnt
-    return p_y < p_x - u * np.sqrt(p_x * (1 - p_x) / x_cnt)
+    count = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt])
+    p_val = proportions_ztest(count, nobs, alternative='larger')[1]
+    return p_val < alpha
